97b7dab2a4555ed3139d4d0d63a6c87121637dcc
tianhuiwen <tianhuiwensir@163.com>
2018-12-18 17:19:26 +0800

    Initial commit

---
+++ sell/src/main/java/com/imooc/sell/SellApplication.java
@@ -0,0 +1,18 @@
+package com.imooc.sell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tianhuiwen
+ * @date 2018/12/18
+ */
+@SpringBootApplication
+public class SellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellApplication.class, args);
+    }
+
+}
+
